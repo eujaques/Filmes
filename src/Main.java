af586e6e16a77069465bd1ada9fa802e7f5b3ba5
eujaques <jaquelinemoraisti@gmail.com>
2025-07-18 15:36:58 -0300

    inicio

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bem vindo ao seu catálogo paticular");
+        System.out.println("Filme: Burlesque");
+
+        int anoDeLancamento = 2010;
+        System.out.println("Lançou em: " + anoDeLancamento);
+    }
+}
